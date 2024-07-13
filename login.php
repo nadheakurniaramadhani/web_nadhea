9209a7fcf39aeac5c26df9f7e4b09f1e37b0cb9e
NADHEAA <149447561+nadheakurniaramadhani@users.noreply.github.com>
2024-07-13 08:11:05 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+    <meta http-equiv="content-type" content="text/html" />
+    <meta name="author" content="lolkittens" />
+
+    <title>LOGIN</title>
+</head>
+
+<body>
+    <form method="post" action="action.php">
+        <input type="text" name="nama_lengkap"/>
+        <input type="submit" value="kirim"/>
+
+    </form>
+    
+</body>
+</html>
